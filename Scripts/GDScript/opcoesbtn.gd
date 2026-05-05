3cca0864b2b993dd36d0f8c15fa9bf9049cf3240
Dieguinho Fox <brfluffyinc@gmail.com>
2026-05-05 13:57:27 -0300

    Create opcoesbtn.gd

---
+++ Scripts/GDScript/opcoesbtn.gd
@@ -0,0 +1,11 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/opcoes.tscn")
